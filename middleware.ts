4e7c68f7e30b0913712a51933cc8f6b8e6dcf226
venkatesh.inamdar <venkateshinamdar28@gmail.com>
2025-09-18 14:30:26 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/dashboard(.*)", // protected
-    "/profile(.*)", // protected
-    "/settings(.*)", // protected
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
